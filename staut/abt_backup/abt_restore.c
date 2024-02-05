422b14c6ea78828a787e65ac7d294414b1eea495
Tomoya Suzuki <t3suzuki@ucsd.edu>
2024-02-05 07:32:52 +0000

    up

--- staut/abt_backup/abt_restore.c
+++ staut/abt_backup/abt_restore.c
@@ -4,28 +4,79 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include "common.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "myfs.h"
 
+superblock_t *superblock;
 
-#define MYFS_BLOCK_SIZE (2*1024*1024)
-#define BUF_SIZE (4096)
+void
+myfs_mount(char *myfs_superblock)
+{
+  int superblock_fd = open(myfs_superblock, O_RDWR);
+  if (superblock_fd < 0) {
+    perror("open");
+  }
+  size_t page_size = getpagesize();
+  struct stat statbuf;
+  fstat(superblock_fd, &statbuf);
+  uint64_t mapped_size = statbuf.st_size;
+  mapped_size = (mapped_size / page_size) * page_size;
+  printf("sizeof(superblock_t)=%ld, mmapped_size=%ld\n", sizeof(superblock_t), mapped_size);
+  superblock = (superblock_t *)mmap(0, mapped_size, PROT_READ|PROT_WRITE, MAP_SHARED, superblock_fd, 0);
+  if (superblock == MAP_FAILED)
+    perror("mmap superblock file");
+}
 
+void
+parse(int ssd_fd, int i)
+{
+  char buf[DEG][BUF_SIZE];
+  int rid[DEG];
+  int j;
+  int n_block = superblock->file[i].n_block;
+  int i_block;
+  for (i_block=0; i_block<n_block; i_block++) {
+    int blk = superblock->file[i].block[i_block];
+    uint64_t lba_base = blk * MYFS_BLOCK_SIZE / 512;
+    uint64_t offset;
+
+    for (j=0; j<DEG; j++) {
+      int ret = read(ssd_fd, buf[j], BUF_SIZE);
+    }
+    
+    for (offset=0; offset<MYFS_BLOCK_SIZE / 512; offset+=8*DEG) {
+      for (j=0; j<DEG; j++) {
+	rid[j] = nvme_write_req(lba_base + offset + 8*j, BUF_SIZE/512, 0, BUF_SIZE, buf[j]);
+      }
+    }
+    
+    for (j=0; j<DEG; j++) {
+      while (1)
+	if (nvme_check(rid[j]))
+	  break;
+    }
+  }
+}
+  
 int
 main()
 {
-  uint64_t lba;
-  char buf[BUF_SIZE];
-  int used_blocks = USED_BLOCKS;
-  
   nvme_init();
-
-  int fd = open("my.dat", O_RDWR|O_CREAT, 0);
-  
-  for (lba=0; lba<used_blocks*MYFS_BLOCK_SIZE/512; lba+=8) {
-    read(fd, buf, BUF_SIZE);
-    int rid = nvme_write_req(lba, BUF_SIZE/512, 0, BUF_SIZE, buf);
-    while (1)
-      if (nvme_check(rid))
-	break;
+  int ssd_fd = open(MY_BACKUP_PATH, O_RDWR|O_CREAT, 0);
+  {
+    int i;
+    myfs_mount("./myfs_superblock");
+    for (i=0; i<MYFS_MAX_FILES; i++) {
+      if (superblock->file[i].name[0] != '\0') {
+	printf("%d %s %lu\n", i, superblock->file[i].name, superblock->file[i].n_block);
+	parse(ssd_fd, i);
+      }
+    }
   }
-  close(fd);
+  close(ssd_fd);
 }
