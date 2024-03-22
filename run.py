82d90c5d6786de64722e9a5ebc2ded2a27916f8e
Tomoya Suzuki <t3suzuki@ucsd.edu>
2024-03-22 23:31:09 +0000

    up

--- run.py
+++ run.py
@@ -11,9 +11,11 @@ MYLIB_PATH = "/home/tomoya-s/mountpoint2/tomoya-s/pthabt/newlib"
 
 def get_db_bench_cmd(mode, op, threads, cache_size, db_path):
     key_size = 32
-    val_size = 512
+    prefix_size = 32
+    val_size = 1024
     #num = 400 * 1000 * 1000
-    num = 30 * 1000 * 1000
+    num = 20 * 1000 * 1000
+    #num = 10
     
     if op == "set":
         benchmarks = "filluniquerandom"
@@ -29,21 +31,25 @@ def get_db_bench_cmd(mode, op, threads, cache_size, db_path):
         block_align = 0
     else:
         block_align = 1
-        
-    cmd = "./db_bench --use_plain_table=0 --stats_per_interval=0 --stats_interval=1048576 --histogram=0  --use_direct_io_for_flush_and_compaction -use_direct_reads --mmap_read=0 --mmap_write=0 --disable_wal=1 --sync=0 --verify_checksum=1 --compression_type=none --compression_ratio=1 --min_level_to_compress=-1 --key_size={} --value_size={} --prefix_size={} --keys_per_prefix=0 --db={} --cache_size={} --cache_numshardbits=12 --cache_index_and_filter_blocks=0 --pin_l0_filter_and_index_blocks_in_cache=0 --cache_high_pri_pool_ratio=0.5 --bloom_bits=10  --level0_file_num_compaction_trigger=8 --level0_slowdown_writes_trigger=16 --level0_stop_writes_trigger=24 --allow_concurrent_memtable_write=false --write_buffer_size=1073741824 --max_write_buffer_number=4 --target_file_size_base=10485760000 --max_bytes_for_level_base=104857600000 --block_size=4096 --block_align={} -use_data_block_hash_index=1 -use_hash_search=1 --file_opening_threads=1 --benchmarks={} --use_existing_db={} --num={} --threads={} ".format(key_size, val_size, key_size, db_path, cache_size, block_align, benchmarks, existing_db, num, threads) + duration
+
+    write_buffer_size = 1073741824 * 4
+    cmd = "./db_bench --stats_per_interval=0 --stats_interval=1048576 --use_direct_io_for_flush_and_compaction -use_direct_reads --mmap_read=0 --mmap_write=0 --disable_wal=1 --sync=0 --verify_checksum=1 --compression_type=none --compression_ratio=1 --min_level_to_compress=-1 --key_size={} --value_size={} --prefix_size={} --keys_per_prefix=0 --db={} --cache_size={} --cache_numshardbits=12 --bloom_bits=-1  --level0_file_num_compaction_trigger=8 --level0_slowdown_writes_trigger=16 --level0_stop_writes_trigger=24 --allow_concurrent_memtable_write=false --write_buffer_size={} --max_write_buffer_number=4 --target_file_size_base=10485760000 --max_bytes_for_level_base=104857600000 --block_size=4096 --block_align={} --file_opening_threads=1 --benchmarks={} --use_existing_db={} --num={} --threads={} -use_data_block_hash_index=0 -use_hash_search=1 ".format(key_size, val_size, prefix_size, db_path, cache_size, write_buffer_size, block_align, benchmarks, existing_db, num, threads) + duration
     return cmd
 
 
 def run(mode, op, n_core, n_th, cache_size):
     print("mode={}, op={}, n_core={}, n_th={}, cache_size={}".format(mode, op, n_core, n_th, cache_size))
     if mode == "abt" or mode == "pthpth":
-        db_path = "/home/tomoya-s/mountpoint2/tomoya-s/rocksdb_abt400m"
-        #db_path = "/home/tomoya-s/mountpoint2/tomoya-s/rocksdb_abt20m"
+        #db_path = "/home/tomoya-s/mountpoint2/tomoya-s/rocksdb_abt20m_test1KB_prefix0"
+        #db_path = "/home/tomoya-s/mountpoint2/tomoya-s/rocksdb_abt20m_test1KB"
+        db_path = "/home/tomoya-s/mountpoint2/tomoya-s/rocksdb_abt20m_test1KB_h"
+        #db_path = "/home/tomoya-s/mountpoint2/tomoya-s/rocksdb_abt10_test1KB_g"
+        #db_path = "/home/tomoya-s/mountpoint2/tomoya-s/rocksdb_abt40m"
     elif mode == "spdk":
         db_path = "/home/tomoya-s/mountpoint2/tomoya-s/rocksdb_spdk4m"
     else:
-        #db_path = "/home/tomoya-s/mountpoint2/tomoya-s/rocksdb_native400m"
-        db_path = "/home/tomoya-s/mountpoint2/tomoya-s/rocksdb_native30m"
+        db_path = "/home/tomoya-s/mountpoint/tomoya-s/rocksdb_native10m_test"
+        #db_path = "/home/tomoya-s/mountpoint/tomoya-s/rocksdb_native30m"
         
     if op == "set":
         print("We are modifying database {}. Are you Sure? (Y/N)".format(db_path))
@@ -54,9 +60,9 @@ def run(mode, op, n_core, n_th, cache_size):
     subprocess.run("sudo chcpu -d {}-39".format(n_core).split())
     
     my_env = os.environ.copy()
-    drive_ids = ["0000:05:00.0","0000:06:00.0"]
+    drive_ids = ["0000:0f:00.0","0000:0e:00.0"]
     if mode == "abt":
-        mylib_build_cmd = "make -C {} ABT_PATH={} N_CORE={} ND={} USE_PREEMPT=1".format(MYLIB_PATH, ABT_PATH, n_core, len(drive_ids))
+        mylib_build_cmd = "make -C {} ABT_PATH={} N_CORE={} ND={} USE_PREEMPT=0".format(MYLIB_PATH, ABT_PATH, n_core, len(drive_ids))
         process = subprocess.run(mylib_build_cmd.split())
         
         my_env["LD_PRELOAD"] = MYLIB_PATH + "/mylib.so"
@@ -101,14 +107,22 @@ def run(mode, op, n_core, n_th, cache_size):
     #print("captured stderr: {}".format(res.stderr.decode()))
 
 #run("native", "set", 1, 1, 1024*1024)
-#run("abt", "set", 1, 1, 1024*1024)
+run("abt", "set", 1, 1, 1024*1024)
 #run("abt", "set", 1, 1, 8*1024*1024)
-#run("abt", "get", 8, 128, 1024*1024)
+#run("abt", "get", 1, 128, 1024*1024)
 #run("native", "set", 1, 1, 1024*1024)
-#run("native", "get", 1, 128, 1024*1024)
-run("io_uring", "get", 8, 128, 1024*1024)
+#run("native", "get", 1, 64, 1024*1024)
+#run("io_uring", "get", 1, 1, 1024*1024)
+#run("io_uring", "set", 1, 1, 1024*1024)
+#run("io_uring", "get", 8, 128, 1024*1024)
+#run("io_uring", "get", 1, 128, 1024*1024)
+#run("native", "get", 8, 256, 1024*1024)
+#run("native", "get", 8, 64, 1024*1024)
 #run("native", "get", 8, 128, 1024*1024)
-#run("abt", "get", 8, 256, 1024*1024)
+#run("abt", "get", 8, 256, 10*1024*1024*1024)
+run("abt", "get", 8, 256, 1*1024*1024)
+#run("abt", "get", 1, 1, 1*1024*1024)
+#run("abt", "get", 1, 256, 1024*1024)
 #run("abt", "get", 8, 128, 1024*1024)
 #run("abt", "get", 8, 128, 12*1024*1024*1024)
 #run("abt", "get", 8, 128, 1024*1024)
@@ -125,7 +139,9 @@ run("io_uring", "get", 8, 128, 1024*1024)
 
 # for n_core in [8]:
 #     for n_pth in [256,512,128,1024,64]:
-#         run("pthpth", "get", n_core, n_pth, 1024*1024)
+#         run("io_uring", "get", n_core, n_pth, 1024*1024)
+#         run("native", "get", n_core, n_pth, 1024*1024)
+        #run("pthpth", "get", n_core, n_pth, 1024*1024)
 
 #for n_core in [8]:
 #    for n_pth in [256,512,128,1024,64]:
--- ycsb-cpp.py
+++ ycsb-cpp.py
@@ -5,17 +5,17 @@ ABT_PATH = "/home/tomoya-s/work/github/ppopp21-preemption-artifact/argobots/inst
 MYLIB_PATH = "/home/tomoya-s/mountpoint2/tomoya-s/pthabt/newlib"
 
 def get_cmd(op, n_th, cache_capacity, workload, dbname, count):
-    common_args = "-db rocksdb -P workloads/{} -P rocksdb/rocksdb.properties -s -p rocksdb.cache_size={} -p rocksdb.dbname={} -p threadcount={}".format(workload, cache_capacity, dbname, n_th)
+    common_args = "-db rocksdb -P workloads/{} -P rocksdb/rocksdb.properties -p rocksdb.cache_size={} -p rocksdb.dbname={} -p threadcount={}".format(workload, cache_capacity, dbname, n_th)
     if op == "set":
-        cmd = "./ycsb -load -p recordcount={} {}".format(count, common_args)
+        cmd = "./ycsb -load {} -p recordcount={} -p status=false".format(common_args, count)
     else:
-        cmd = "./ycsb -run -p recordcount={} {}".format(count, common_args)
+        cmd = "./ycsb -run {} -p operationcount={} -p status=false".format(common_args, count)
     return cmd
 
 def run(mode, op, n_core, n_th, cache_capacity, workload, count):
     print("mode={}, op={}, n_core={}, n_th={}, cache_size={}".format(mode, op, n_core, n_th, cache_capacity))
 
-    drive_ids = ["0000:0d:00.0","0000:0e:00.0"]
+    drive_ids = ["0000:0f:00.0","0000:0e:00.0"]
     
     if mode == "abt" or mode == "pthpth":
         db_path = "/home/tomoya-s/mountpoint2/tomoya-s/ycsb-rocks-abt"
@@ -35,22 +35,22 @@ def run(mode, op, n_core, n_th, cache_capacity, workload, count):
     else:
         use_sched_yield = 0
 
-    subprocess.run("make compact -B N_TH={} DB_PATH={}".format(n_th, db_path).split())
-    if mode == "abt":
-        mylib_build_cmd = "make -C {} ABT_PATH={} N_CORE={} ND={} USE_PREEMPT=0".format(MYLIB_PATH, ABT_PATH, n_core, len(drive_ids))
-        process = subprocess.run(mylib_build_cmd.split())
+    if op == "get":
+        subprocess.run("make -f Makefile.compact compact -B N_TH={} DB_PATH={}".format(n_th, db_path).split())
+        if mode == "abt":
+            mylib_build_cmd = "make -C {} ABT_PATH={} N_CORE={} ND={} USE_PREEMPT=0".format(MYLIB_PATH, ABT_PATH, 1, len(drive_ids))
+            process = subprocess.run(mylib_build_cmd.split())
         
-        comp_env = os.environ.copy()
-        comp_env["LD_PRELOAD"] = MYLIB_PATH + "/mylib.so"
-        comp_env["LD_LIBRARY_PATH"] = ABT_PATH + "/lib:/home/tomoya-s/mountpoint2/tomoya-s/rocksdb/build"
-        comp_env["HOOKED_ROCKSDB_DIR"] = db_path
-        comp_env["DRIVE_IDS"] = "_".join(drive_ids)
-        comp_env["MYFS_SUPERBLOCK_PATH"] = "/root/myfs_superblock"
-        #res = subprocess.run("./compact", env=comp_env)
-    else:
-        res = subprocess.run("./compact")
+            comp_env = os.environ.copy()
+            comp_env["LD_PRELOAD"] = MYLIB_PATH + "/mylib.so"
+            comp_env["LD_LIBRARY_PATH"] = ABT_PATH + "/lib:/home/tomoya-s/mountpoint2/tomoya-s/rocksdb/build"
+            comp_env["HOOKED_ROCKSDB_DIR"] = db_path
+            comp_env["DRIVE_IDS"] = "_".join(drive_ids)
+            comp_env["MYFS_SUPERBLOCK_PATH"] = "/root/myfs_superblock"
+            res = subprocess.run("./compact", env=comp_env)
+        else:
+            res = subprocess.run("./compact")
         
-    subprocess.run("make N_TH={} DB_PATH={} USE_SCHED_YIELD={} SET_DATA={} CACHE_CAPACITY={}".format(n_th, db_path, use_sched_yield, set_data, cache_capacity).split())
     subprocess.run("sudo chcpu -e 1-{}".format(n_core-1).split())
     subprocess.run("sudo chcpu -d {}-39".format(n_core).split())
     
@@ -83,6 +83,24 @@ def run(mode, op, n_core, n_th, cache_capacity, workload, count):
     #print("captured stdout: {}".format(res.stdout.decode()))
     #print("captured stderr: {}".format(res.stderr.decode()))
 
-run("native", "get", 10, 10, 1*1024*1024*1024, "workloada", 1000*1000)
+#run("abt", "set", 1, 1,    1*1024*1024*1024, "workloada", 1000*1000*4)
+#run("abt", "get", 10, 500, 1*1024*1024*1024, "workloada", 1000*1000*4)
+
+#run("abt", "set", 10, 100, 1*1024*1024*1024, "workloadb", 1000*1000*10)
+#run("abt", "get", 10, 100, 1*1024*1024*1024, "workloadb", 1000*1000*10)
+
+#run("abt", "set", 1, 1,    1*1024*1024*1024, "workloadc", 1000*1000*10)
+run("abt", "get", 8, 128, 1*1024*1024, "workloadc", 1000*1000*10)
+#run("abt", "get", 8, 128, 1*1024*1024, "workloadc", 1000*1000*8)
+#run("native", "set", 1, 1,    1*1024*1024*1024, "workloadc", 1000*1000*10)
+#run("native", "get", 8, 128, 1*1024*1024, "workloadc", 1000*1000*10)
+#run("native", "get", 8, 128, 1*1024*1024, "workloadc", 1000*1000*1)
+#run("native", "get", 1, 128, 1*1024*1024, "workloadc", 1000*1000*1)
+
+#run("native", "get", 10, 100, 1*1024*1024*1024, "workloadb", 1000*1000*10)
+#run("native", "get", 10, 100, 1*1024*1024*1024, "workloadc", 1000*1000*10)
+#run("native", "get", 10, 100, 1*1024*1024*1024, "workloadd", 1000*1000*10)
+#run("native", "get", 10, 100, 1*1024*1024*1024, "workloade", 1000*1000*10)
+#run("native", "get", 10, 100, 1*1024*1024*1024, "workloadf", 1000*1000*10)
 #run("io_uring", "get", 10, 500, 1024*1024*1024)
 #run("abt", "get", 10, 100, 2*1024*1024*1024)
